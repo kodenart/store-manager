0bf9c15adfb42f7c224777ec826075c301be2a2b
kodenart <mjuanvitorp@gmail.com>
2022-06-03 17:06:31 -0300

    feat(app): adding middlewares

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const productsRoute = require('./routes/productsRoute');
 const salesRoute = require('./routes/salesRoute');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/products', productsRoute);
 app.use('/sales', salesRoute);
+app.use('/products', productsRoute);
+
+app.use(errorHandler);
 
 module.exports = app;
