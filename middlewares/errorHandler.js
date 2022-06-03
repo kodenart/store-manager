ac3c16cb745b1e887590b2b179dc6e0e541968eb
kodenart <mjuanvitorp@gmail.com>
2022-06-03 19:06:42 -0300

    feat(utils): http status helper

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,4 @@
-const httpStatus = {
-  BAD_REQUEST: 400,
-  NOT_FOUND: 404,
-  UNPROCESSABLE_ENTITY: 422,
-  INTERNAL_ERROR: 500,
-};
+const httpStatus = require('../utils/httpStatus');
 
 const errorHandler = (err, req, res, _next) => {
   if (err.isJoi) {
---
+++ utils/httpStatus.js
@@ -0,0 +1,9 @@
+const httpStatus = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  UNPROCESSABLE_ENTITY: 422,
+  CONFLICT: 409,
+  INTERNAL_ERROR: 500,
+};
+
+module.exports = httpStatus;
