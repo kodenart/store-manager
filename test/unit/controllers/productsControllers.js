758c758798fc408419526472b6eb8e4caa1bbe26
kodenart <mjuanvitorp@gmail.com>
2022-06-07 14:57:34 -0300

    test(controllers): 100% coverage on this layer

--- test/unit/controllers/productsControllers.js
+++ test/unit/controllers/productsControllers.js
@@ -4,42 +4,141 @@ const productsService = require('../../../services/productsService')
 const productsController = require('../../../controllers/productsController')
 
 
-describe('Unit tests getAll and getById product controllers', () => {
-  const idParam = 1
+describe('Unit tests all methods from product controllers when nothing goes wrong', () => {
   const res = {}
   const req = {}
-  const getAllResolve = [{id: 1}, {id: 2}, {id: 3}]
-  const getByIdResolve = getAllResolve.filter((e) => e.id === idParam)
+  const resolveAll = [{id: 1}, {id: 2}, {id: 3}]
+  const resolveById = [{id: 1}]
+  const resolveAdd = {key: 'value'}
+  const resolveUpdate = {id: 1, name: 'ProductName', quantity: 42}
+  const resolveExclude = {}
+  const resolveError = {code: 'anything', message: 'anything'};
 
   before(() => {
-    sinon.stub(productsService, 'getAll').resolves(getAllResolve)
-    sinon.stub(productsService, 'getById').resolves(getByIdResolve)
+    sinon.stub(productsService, 'getAll').resolves(resolveAll)
+    sinon.stub(productsService, 'getById').resolves(resolveById)
+    sinon.stub(productsService, 'add').resolves(resolveAdd)
+    sinon.stub(productsService, 'update').resolves(resolveUpdate)
+    sinon.stub(productsService, 'exclude').resolves(resolveExclude)
 
     res.status = sinon.stub().returns(res)
     res.json = sinon.stub().returns()
+    res.end = sinon.stub().returns()
     
   })
 
   after(() => {
     productsService.getAll.restore()
     productsService.getById.restore()
+    productsService.add.restore()
+    productsService.update.restore()
+    productsService.exclude.restore()
   })
 
   describe('Testing the getAll method', () => {
-    it('calls status with 200 and json with anArrOfObj', async () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
       await productsController.getAll(req, res)
       expect(res.status.calledWith(200)).to.be.equal(true)
-      expect(res.json.calledWith(getAllResolve)).to.be.equal(true)
+      expect(res.json.calledWith(resolveAll)).to.be.equal(true)
     })
   })
 
   describe('Testing the getById method', () => {
-    it('it returns an array (collection) of resources', async () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
       req.params = { id: 1 };
       await productsController.getById(req, res)
       expect(res.status.calledWith(200)).to.be.equal(true)
-      expect(res.json.calledWith(getByIdResolve)).to.be.equal(true)
+      expect(res.json.calledWith(resolveById)).to.be.equal(true)
     })
 
+  })
+
+  describe('Testing the add method', () => {
+    it('calls status with 201 and json with what is resolved by the service', async () => {
+      req.body = {name: 'ProductName', quantity: 42}
+      await productsController.add(req, res)
+      expect(res.status.calledWith(201)).to.be.equal(true)
+      expect(res.json.calledWith(resolveAdd)).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the update method', () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
+      req.params = { id: 1 }
+      req.body = { name: 'ProductName', quantity: 42 }
+      await productsController.update(req, res)
+      expect(res.status.calledWith(200)).to.be.equal(true)
+      // expect(res.json.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the exclude method', () => {
+    it('calls status with 204 and end despite what is returned from the service', async () => {
+      req.params = { id: 1 }
+      await productsController.exclude(req, res)
+      expect(res.status.calledWith(204)).to.be.equal(true)
+      // expect(res.end.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
 })
+
+
+describe('Unit tests all methods from product controllers when something goes wrong', () => {
+  const res = {}
+  const req = {}
+  let next = () => {}
+  const resolveError = {code: 'anything', message: 'anything'};
+
+  before(() => {
+    sinon.stub(productsService, 'getById').resolves(resolveError)
+    sinon.stub(productsService, 'add').resolves(resolveError)
+    sinon.stub(productsService, 'update').resolves(resolveError)
+    sinon.stub(productsService, 'exclude').resolves(resolveError)
+
+    next = sinon.stub().returns()
+  })
+
+  after(() => {
+    productsService.getById.restore()
+    productsService.add.restore()
+    productsService.update.restore()
+    productsService.exclude.restore()
+  })
+
+  describe('Testing the getById method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 };
+      await productsController.getById(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+    })
+
+  })
+
+  describe('Testing the add method', () => {
+    it('should call next with the error obj', async () => {
+      req.body = {name: 'ProductName', quantity: 42}
+      await productsController.add(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the update method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 }
+      req.body = { name: 'ProductName', quantity: 42 }
+      await productsController.update(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+      // expect(res.json.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the exclude method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 }
+      await productsController.exclude(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+      // expect(res.end.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
 })
--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -4,42 +4,140 @@ const salesService = require('../../../services/salesService')
 const salesController = require('../../../controllers/salesController')
 
 
-describe('Unit tests getAll and getById sale controllers', () => {
-  const idParam = 1
+describe('Unit tests all methods from sales controllers when nothing goes wrong', () => {
   const res = {}
   const req = {}
-  const getAllResolve = [{id: 1}, {id: 1}, {id: 2}]
-  const getByIdResolve = getAllResolve.filter((e) => e.id === idParam)
+  const resolveAll = [{id: 1}, {id: 2}, {id: 3}]
+  const resolveById = [{id: 1}]
+  const resolveAdd = {key: 'value'}
+  const resolveUpdate = {id: 1, ItemsUpdated: [{name: 'ProductName', quantity: 42}, {name: 'ProductName', quantity: 42}]}
+  const resolveExclude = {}
 
   before(() => {
-    sinon.stub(salesService, 'getAll').resolves(getAllResolve)
-    sinon.stub(salesService, 'getById').resolves(getByIdResolve)
+    sinon.stub(salesService, 'getAll').resolves(resolveAll)
+    sinon.stub(salesService, 'getById').resolves(resolveById)
+    sinon.stub(salesService, 'createSale').resolves(resolveAdd)
+    sinon.stub(salesService, 'updateSales').resolves(resolveUpdate)
+    sinon.stub(salesService, 'excludeSale').resolves(resolveExclude)
 
     res.status = sinon.stub().returns(res)
     res.json = sinon.stub().returns()
+    res.end = sinon.stub().returns()
     
   })
 
   after(() => {
     salesService.getAll.restore()
     salesService.getById.restore()
+    salesService.createSale.restore()
+    salesService.updateSales.restore()
+    salesService.excludeSale.restore()
   })
 
   describe('Testing the getAll method', () => {
-    it('it returns an array (collection) of resources', async () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
       await salesController.getAll(req, res)
       expect(res.status.calledWith(200)).to.be.equal(true)
-      expect(res.json.calledWith(getAllResolve)).to.be.equal(true)
+      expect(res.json.calledWith(resolveAll)).to.be.equal(true)
     })
   })
 
   describe('Testing the getById method', () => {
-    it('it returns an array (collection) of resources', async () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
       req.params = { id: 1 };
       await salesController.getById(req, res)
       expect(res.status.calledWith(200)).to.be.equal(true)
-      expect(res.json.calledWith(getByIdResolve)).to.be.equal(true)
+      expect(res.json.calledWith(resolveById)).to.be.equal(true)
     })
 
+  })
+
+  describe('Testing the add method', () => {
+    it('calls status with 201 and json with what is resolved by the service', async () => {
+      req.body = {name: 'ProductName', quantity: 42}
+      await salesController.create(req, res)
+      expect(res.status.calledWith(201)).to.be.equal(true)
+      expect(res.json.calledWith(resolveAdd)).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the update method', () => {
+    it('calls status with 200 and json with what is resolved by the service', async () => {
+      req.params = { id: 1 }
+      req.body = { name: 'ProductName', quantity: 42 }
+      await salesController.update(req, res)
+      expect(res.status.calledWith(200)).to.be.equal(true)
+      // expect(res.json.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the exclude method', () => {
+    it('calls status with 204 and end despite what is returned from the service', async () => {
+      req.params = { id: 1 }
+      await salesController.exclude(req, res)
+      expect(res.status.calledWith(204)).to.be.equal(true)
+      // expect(res.end.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
 })
+
+
+describe('Unit tests all methods from sales controllers when something goes wrong', () => {
+  const res = {}
+  const req = {}
+  let next = () => {}
+  const resolveError = {code: 'anything', message: 'anything'};
+
+  before(() => {
+    sinon.stub(salesService, 'getById').resolves(resolveError)
+    sinon.stub(salesService, 'createSale').resolves(resolveError)
+    sinon.stub(salesService, 'updateSales').resolves(resolveError)
+    sinon.stub(salesService, 'excludeSale').resolves(resolveError)
+
+    next = sinon.stub().returns()
+  })
+
+  after(() => {
+    salesService.getById.restore()
+    salesService.createSale.restore()
+    salesService.updateSales.restore()
+    salesService.excludeSale.restore()
+  })
+
+  describe('Testing the getById method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 };
+      await salesController.getById(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+    })
+
+  })
+
+  describe('Testing the add method', () => {
+    it('should call next with the error obj', async () => {
+      req.body = {name: 'ProductName', quantity: 42}
+      await salesController.create(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the update method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 }
+      req.body = { name: 'ProductName', quantity: 42 }
+      await salesController.update(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+      // expect(res.json.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
+  describe('Testing the exclude method', () => {
+    it('should call next with the error obj', async () => {
+      req.params = { id: 1 }
+      await salesController.exclude(req, res, next)
+      expect(next.calledWith(resolveError)).to.be.equal(true)
+      // expect(res.end.toHaveBeenCalled()).to.be.equal(true)
+    })
+  })
+
 })
