8e77563a236cc61d350806cbee79d5a030a71edc
kodenart <mjuanvitorp@gmail.com>
2022-06-08 10:11:15 -0300

    test(models): almost 100% of this layer has coverage

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -48,3 +48,72 @@ describe('Unit testing the productModel', () => {
   })
 
 })
+
+
+
+describe('Unit testing the rest of productModel', () => {
+  describe('Testing the add method', () => {
+  before(async () => {
+    const execute = [{ insertId: 1 }, [], []]; 
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+    it('returns an insertId', async () => {
+      const response = await productsModel.add('Idk', 5)
+      expect(response).to.be.equal(1)
+    })
+  })
+
+  describe('Testing the update method', () => {
+  before(async () => {
+    const execute = [{ affectedRows: 1 }, [], []]; 
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+    it('returns an insertId', async () => {
+      const response = await productsModel.update(1, 'Idk', 5)
+      expect(response).to.be.equal(1)
+    })
+  })
+
+  describe('Testing the exclude method', () => {
+  before(async () => {
+    const execute = [{}, [], []]; 
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+    it('returns an insertId', async () => {
+      const response = await productsModel.exclude(3)
+      expect(response).to.be.equal(0)
+    })
+  })
+
+  describe('Testing the updateQnt method', () => {
+  before(async () => {
+    const execute = ['zzz', [], []]; 
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  after(async () => {
+    connection.execute.restore();
+  });
+    it('returns an insertId', async () => {
+      const response = await productsModel.updateQnt({productId: 2, quantity: 5}, 'add')
+      expect(response).to.be.equal('zzz')
+    })
+  })
+
+})
--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -7,7 +7,7 @@ const connection = require('../../../models/connection')
 describe('Unit testing the saleModel', () => {
 
   before(async () => {
-    const execute = [[{ a: 1 }, { b: 2 }, { c: 3 }], [], []]; 
+    const execute = [[{ a: 1, insertId: 1 }, { b: 2 }, { c: 3 }], [], []]; 
 
     sinon.stub(connection, 'execute').resolves(execute);
   });
@@ -30,13 +30,35 @@ describe('Unit testing the saleModel', () => {
       expect(response).to.be.an('array')
       expect(response).to.have.length.above(0)
     })
+  })
 
-    // it('If no parameters are passed, an error is returned', async () => {
-    //   const response = await saleModel.getById()
-    //   expect(response).to.be.an('object')
-    //   expect(response).to.have.property('code')
-    //   expect(response).to.have.property('message')
-    // })
+  describe('Testing the createSaleId method', () => {
+    it('it returns an array (collection) of resources', async () => {
+      const response = await salesModel.createSaleId(1)
+      expect(response).to.be.equal(undefined)
+    })
+  })
+
+  describe('Testing the createSaleProduct method', () => {
+    it('it returns an array (collection) of resources', async () => {
+      const response = await salesModel.createSaleProduct(1, 2, 3)
+      expect(response).to.be.an('array')
+      expect(response).to.have.length.above(0)
+    })
+  })
+
+  describe('Testing the deleteSales method', () => {
+    it('it returns an array (collection) of resources', async () => {
+      const response = await salesModel.deleteSales(1)
+      expect(response).to.be.equal(0)
+    })
+  })
+
+  describe('Testing the excludeSale method', () => {
+    it('it returns an array (collection) of resources', async () => {
+      const response = await salesModel.excludeSale(1)
+      expect(response).to.be.equal(0)
+    })
   })
 
 })
